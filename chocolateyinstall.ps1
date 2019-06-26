dac38f2d93ffe0cd2d4187febd5239e18a30387a
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2019-06-26 13:02:26 -0700

    Add files via upload

---
+++ chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'scinstalls.exe'
+$packageName = 'Soft Cleaner'
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  file         = $fileLocation
+  softwareName  = 'Soft Cleaner'
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'  
+}
+Install-ChocolateyInstallPackage @packageArgs
