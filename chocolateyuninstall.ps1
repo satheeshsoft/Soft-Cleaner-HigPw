2618f290107af00c42e67047d31c00adff13d748
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2024-03-20 03:16:04 +0530

    Update chocolateyuninstall.ps1

--- chocolateyuninstall.ps1
+++ chocolateyuninstall.ps1
@@ -1,7 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageArgs = @{
   Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
+  Write-Warning "Following keys were matched:"
   $key | % {Write-Warning "- $($_.DisplayName)"}
 }
